796d12e64c133bc3b865e6595f0248a9c57f01ef
Diuke <juan-pablo-duque@hotmail.com>
2020-03-19 19:28:14 +0100

    como gc added and other things

--- cw1/LC2GC.m
+++ cw1/LC2GC.m
@@ -1,7 +1,8 @@
-function [outputArg1,outputArg2] = LC2GC(inputArg1,inputArg2)
-%LC2GC Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+function [ gc ] = LC2GC(x0, enu)
+    geodetic = cart2geo(x0);
+    phi = geodetic(1);lam = geodetic(2);h = geodetic(3);
+    R = [-sin(lam) cos(lam) 0;-sin(phi)*cos(lam) -sin(phi)*sin(lam) cos(phi);cos(phi)*cos(lam) cos(phi)*sin(lam) sin(phi)]
+    
+    gc = R' * enu + x0;
 end
 
--- cw1/RotLL2LC.m
+++ cw1/RotLL2LC.m
@@ -1,7 +1,11 @@
-function [ output_args ] = RotLL2LC( input_args )
-%ROTLL2LC Summary of this function goes here
-%   Detailed explanation goes here
-
-
+function [ R ] = RotLL2LC( x_lc1, xi, eta )
+    Rx = [1 0 0;0 cos(xi) -sin(xi);0 sin(xi) cos(xi)];
+    Ry = [cos(eta) 0 -sin(eta);0 1 0;sin(eta) 0 cos(eta)];
+    
+    xtemp = Ry * Rx * x_lc1;
+    alpha = atan(xtemp(2)/xtemp(1));
+    
+    Rz = [cos(alpha) sin(alpha) 0;-sin(alpha) cos(alpha) 0; 0 0 1];
+    R = Rz * Ry * Rx;
 end
 
