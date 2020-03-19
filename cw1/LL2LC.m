4c04b8fa113c240aa8bcc57c56a4bc3d19d717f3
anpugliese <angellyp@uninorte.edu.co>
2020-03-19 20:03:17 +0100

    WF3

--- cw1/LL2LC.m
+++ cw1/LL2LC.m
@@ -1,7 +1,12 @@
-function [outputArg1,outputArg2] = LL2LC(inputArg1,inputArg2)
-%LL2LC Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+function [ LC ] = LL2LC(LL, eta, xi)
+
+Rx = [1 0 0; 0 1 -xi; 0 xi 1];
+Ry = [1 0 -eta; 0 1 0; eta 0 1];
+Rtemp = 
+
+alpha = atan(LL(1)/LL(2));
+Rz = [cos(alpha) sin(alpha) 0; -sin(alpha) cos(alpha) 0; 0 0 1];
+
+
 end
 
