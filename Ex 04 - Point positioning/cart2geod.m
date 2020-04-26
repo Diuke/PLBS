62655a9dfeb579b115e189e05d8c4a4a621607c9
anpugliese <angellyp@uninorte.edu.co>
2020-04-26 20:12:47 +0200

    nonlinear least squares working

--- Ex 04 - Point positioning/cart2geod.m
+++ Ex 04 - Point positioning/cart2geod.m
@@ -29,12 +29,12 @@ r = sqrt(X.^2 + Y.^2 + Z.^2);
 lam = atan2(Y,X);
 
 %geocentric latitude
-phiC = atan(Z./sqrt(X.^2 + Y.^2));
+phiC = atan2(Z,sqrt(X.^2 + Y.^2));
 
 %coordinate transformation
-psi = atan(tan(phiC)/sqrt(1-e^2));
+psi = atan2(tan(phiC),sqrt(1-e^2));
 
-phi = atan((r.*sin(phiC) + e^2*a/sqrt(1-e^2) * (sin(psi)).^3) ./ ...
+phi = atan2((r.*sin(phiC) + e^2*a/sqrt(1-e^2) * (sin(psi)).^3) , ...
     			(r.*cos(phiC) - e^2*a * (cos(psi)).^3));
 
 N = a ./ sqrt(1 - e^2 * sin(phi).^2);
---
+++ Ex 04 - Point positioning/tropo_error.m
@@ -0,0 +1,13 @@
+function [error] = tropo_error(eta,ellipsoidal_height)
+    Hr = 0.0; %/* [m] */
+    Pr = 1013.25; %/* [mb] */
+    Tr =18; %/* [°C] */
+    hr= 0.5; % /* [%-->50%] */                     
+    P = (Pr) * ((1 - 0.0000226*(ellipsoidal_height - (Hr) )^5.225));
+    T = (Tr) - 0.0065*(ellipsoidal_height-(Hr)); %  /* [C] */
+    h = (hr) * exp(-0.0006396*(ellipsoidal_height-(Hr)));
+    Trp_delay = (0.002277/sin(eta)) * (P + (1255/(T+273.15) + 0.05)*h - 1./(tan(eta).^2));
+    
+    error = Trp_delay;
+end
+
